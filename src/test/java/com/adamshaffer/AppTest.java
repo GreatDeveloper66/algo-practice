0c387be1addbe163f0cbf5860d99d3d499573669
GreatDeveloper66 <ashaffe@gmail.com>
2026-06-26 16:55:45 -0500

    change to jupiter import and change access modifiers

--- src/test/java/com/adamshaffer/AppTest.java
+++ src/test/java/com/adamshaffer/AppTest.java
@@ -1,19 +1,20 @@
 package com.adamshaffer;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+
+class AppTest 
 {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    void shouldAnswerWithTrue()
     {
         assertTrue( true );
     }
