c311eed6d3eeabecae11dde97082a6a6317e6ecd
GreatDeveloper66 <ashaffe@gmail.com>
2026-06-26 16:54:13 -0500

    Build ListCalculationsInterface

---
+++ src/main/java/com/adamshaffer/ListCalculationsInterface.java
@@ -0,0 +1,12 @@
+package com.adamshaffer;
+
+public interface ListCalculationsInterface {
+    Integer sum(Integer[] list);
+    Integer min(Integer[] list);
+    Integer max(Integer[] list);
+    Integer average(Integer[] list);
+    Integer median(Integer[] list);
+    Integer mode(Integer[] list);
+    Integer[]sort(Integer[] list);
+
+}
