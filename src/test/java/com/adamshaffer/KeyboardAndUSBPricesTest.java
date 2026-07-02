d6f39bc095918d8710287dd44e5966200a9d75db
GreatDeveloper66 <ashaffe@gmail.com>
2026-07-02 10:00:00 -0500

    Add setup mehtod to KeyboardAndUSBPrices test file

--- src/test/java/com/adamshaffer/KeyboardAndUSBPricesTest.java
+++ src/test/java/com/adamshaffer/KeyboardAndUSBPricesTest.java
@@ -1,5 +1,9 @@
 package com.adamshaffer;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class KeyboardAndUSBPricesTest {
-    
+    @BeforeEach
+    public void setUp() throws Exception {
+    }
 }
