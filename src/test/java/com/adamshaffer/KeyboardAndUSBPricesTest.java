7605adef751467dbae47578923d4640a3ae12e80
GreatDeveloper66 <ashaffe@gmail.com>
2026-07-03 10:00:00 -0500

    Add private variable KeyboardAndUSBPrices to KeyboardAndUSBPricesTest

--- src/test/java/com/adamshaffer/KeyboardAndUSBPricesTest.java
+++ src/test/java/com/adamshaffer/KeyboardAndUSBPricesTest.java
@@ -3,6 +3,7 @@ package com.adamshaffer;
 import org.junit.jupiter.api.BeforeEach;
 
 public class KeyboardAndUSBPricesTest {
+    private KeyboardAndUSBPrices keyboardAndUSBPrices;
     @BeforeEach
     public void setUp() throws Exception {
     }
