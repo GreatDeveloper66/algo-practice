d9bf4d29ceac01e3221c2cb65f2d29e19a754132
GreatDeveloper66 <ashaffe@gmail.com>
2026-07-04 10:00:00 -0500

    Create PriceList private vars

--- src/test/java/com/adamshaffer/KeyboardAndUSBPricesTest.java
+++ src/test/java/com/adamshaffer/KeyboardAndUSBPricesTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 
 public class KeyboardAndUSBPricesTest {
     private KeyboardAndUSBPrices keyboardAndUSBPrices;
+    private PriceList priceListA;
+    private PriceList priceListB;
     @BeforeEach
     public void setUp() throws Exception {
         keyboardAndUSBPrices = new KeyboardAndUSBPrices(new PriceList(new Integer[]{40, 50, 60}), new PriceList(new Integer[]{5, 8, 9}), 60);
